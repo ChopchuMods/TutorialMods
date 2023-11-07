920516f3569331a15b8b4fb69ae36297210d6e8b
Chopchu <kotlewski.kacper@gmail.com>
2023-11-08 00:56:56 +0100

    basic setup

--- src/main/java/pl/kotlewski/tutorialmod/TutorialMod.java
+++ src/main/java/pl/kotlewski/tutorialmod/TutorialMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import pl.kotlewski.tutorialmod.item.ModItems;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
@@ -11,10 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
